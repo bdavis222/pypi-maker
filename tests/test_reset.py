e37c8f9527a59c758cdd3a5ba0fd74549dde6e0d
Brian Davis <briandavis03@gmail.com>
2023-01-06 22:05:28 -0700

    Updated: tests

---
+++ tests/test_reset.py
@@ -0,0 +1,12 @@
+import unittest
+import src.reset as reset
+
+# This unit test uses Python's built-in unit testing framework
+# See https://docs.python.org/3/library/unittest.html for more information
+
+class ResetTest(unittest.TestCase):
+    def test_condition_doesThis_whenThis(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_upload.py
@@ -0,0 +1,12 @@
+import unittest
+import src.upload as upload
+
+# This unit test uses Python's built-in unit testing framework
+# See https://docs.python.org/3/library/unittest.html for more information
+
+class UploadTest(unittest.TestCase):
+    def test_condition_doesThis_whenThis(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
